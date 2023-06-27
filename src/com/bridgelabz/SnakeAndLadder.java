a7b5c5693292d427c29ca2272ed63e669af58c69
Supriya Bhurle <supriyabhurle28@gmail.com>
2023-06-27 10:17:28 +0530

    Added : Rpll the dice

--- src/com/bridgelabz/SnakeAndLadder.java
+++ src/com/bridgelabz/SnakeAndLadder.java
@@ -1,7 +1,13 @@
 package com.bridgelabz;
 
+import java.util.Random;
+
 public class SnakeAndLadder {
     public static void main(String[] args) {
         System.out.println("Welcome to Snake and Ladder Problem");
+        Random random = new Random();
+        int diceRoll = random.nextInt(7);
+
+        System.out.println("current position : "+diceRoll);
     }
 }
