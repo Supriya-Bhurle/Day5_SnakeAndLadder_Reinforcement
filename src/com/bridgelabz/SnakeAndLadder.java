a174fa4d83f94e6e0d203e7dd19c270f29c5e8f5
Supriya Bhurle <supriyabhurle28@gmail.com>
2023-06-27 10:25:53 +0530

    Added : Player check the option

--- src/com/bridgelabz/SnakeAndLadder.java
+++ src/com/bridgelabz/SnakeAndLadder.java
@@ -4,16 +4,31 @@ import java.util.Random;
 
 public class SnakeAndLadder {
     static public final int Starting_Position_Of_Player = 0;
+    static public final int Ending_Position_Of_Player = 100;
+    public static final int Ladder = 2;
+    public static final int Snake = 1;
+    public static final int NoPlay = 0;
+    public static int rolldie(){
+        int dieValue =(int)Math.floor(Math.random()*10)%6+1;
+        return dieValue;
+    }
     public static void main(String[] args) {
-        System.out.println("Welcome to Snake and Ladder Problem");
- uc2_RollTheDice
-        Random random = new Random();
-        int diceRoll = random.nextInt(7);
+        System.out.println("welcome to snake and ladder program");
 
-        System.out.println("current position : "+diceRoll);
-
-        System.out.println("Current Position of player : " + Starting_Position_Of_Player);
-
+        int rolldie = rolldie();
+        int currentPosition = 0;
+        int checkPosition= (int)Math.floor(Math.random()*10)%3;
+        switch (checkPosition){
+            case Snake:
+                currentPosition = rolldie - Snake;
+                System.out.println("player is on Snake " +checkPosition);
+                break;
+            case  Ladder:
+                currentPosition = Ladder + rolldie;
+                System.out.println("player is on Ladder " +currentPosition);
+                break;
+            default:
+                System.out.println("player is not playing " );
 
     }
-}
+}}
